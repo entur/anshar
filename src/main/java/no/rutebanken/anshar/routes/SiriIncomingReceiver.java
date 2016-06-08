05aadeeda9a29451329a2a3c7f15b1e16eb09821
Lasse Tyrihjell <lassetyr@gmail.com>
2016-06-08 08:24:26 +0200

    Standardized queue-naming Added endpoint for retrieving current SX-data (for testing)

--- src/main/java/no/rutebanken/anshar/routes/SiriIncomingReceiver.java
+++ src/main/java/no/rutebanken/anshar/routes/SiriIncomingReceiver.java
@@ -1,5 +1,6 @@
 package no.rutebanken.anshar.routes;
 
+import no.rutebanken.anshar.messages.Situations;
 import no.rutebanken.anshar.routes.handlers.SiriHandler;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
@@ -7,13 +8,22 @@ import org.apache.camel.builder.xml.Namespaces;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.xml.bind.JAXBException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class SiriIncomingReceiver extends RouteBuilder {
-
     private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private final String QUEUE_PREFIX              = "anshar.siri";
+    private final String TRANSFORM_QUEUE           = QUEUE_PREFIX + ".transform";
+    private final String ROUTER_QUEUE              = QUEUE_PREFIX + ".router";
+    private final String DEFAULT_PROCESSOR_QUEUE   = QUEUE_PREFIX + ".process";
+    private final String SITUATION_EXCHANGE_QUEUE  = DEFAULT_PROCESSOR_QUEUE + ".sx";
+    private final String VEHICLE_MONITORING_QUEUE  = DEFAULT_PROCESSOR_QUEUE + ".vm";
+    private final String ESTIMATED_TIMETABLE_QUEUE = DEFAULT_PROCESSOR_QUEUE + ".et";
+    private final String HEARTBEAT_QUEUE           = DEFAULT_PROCESSOR_QUEUE + ".heartbeat";
 
     private String inboundPort;
     private String incomingPathPattern;
@@ -29,50 +39,64 @@ public class SiriIncomingReceiver extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
+
+        //Incoming notifications/deliveries
+        from("netty4-http:http://0.0.0.0:" + inboundPort + "/rest/sx")
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant("200"))
+                .process(p -> {
+                    try {
+                        p.getOut().setBody(SiriObjectFactory.toXml(SiriObjectFactory.createSiriObject(Situations.getAll())));
+                    } catch (JAXBException e1) {
+                        e1.printStackTrace();
+                    }
+                })
+        ;
+
+
         //Incoming notifications/deliveries
         from("netty4-http:http://0.0.0.0:" + inboundPort + "?matchOnUriPrefix=true")
                 .setHeader("anshar.message.id", constant(UUID.randomUUID().toString()))
                 .to("log:received:" + getClass().getSimpleName() + "?showAll=true&multiline=true")
-                .to("activemq:queue:anshar.siri.transform?disableReplyTo=true")
+                .to("activemq:queue:" + TRANSFORM_QUEUE + "?disableReplyTo=true")
                 .setHeader(Exchange.HTTP_RESPONSE_CODE, constant("202"))
                 .setBody(constant(null))
         ;
 
-        from("activemq:queue:anshar.siri.transform?asyncConsumer=true")
+        from("activemq:queue:" + TRANSFORM_QUEUE + "?asyncConsumer=true")
                 .to("log:transformer:" + getClass().getSimpleName() + "?showAll=true&multiline=true")
                 .to("xslt:xsl/siri_soap_raw.xsl?saxon=true") // Extract SOAP version and convert to raw SIRI
                 .to("xslt:xsl/siri_14_20.xsl?saxon=true") // Convert from v1.4 to 2.0
                 .to("file:" + incomingLogDirectory)
                 .to("log:received transformed:" + getClass().getSimpleName() + "?showAll=true&multiline=true")
-                .to("activemq:queue:anshar.siri.router")
+                .to("activemq:queue:" + ROUTER_QUEUE)
         ;
 
 
         Namespaces ns = new Namespaces("siri", "http://www.siri.org.uk/siri")
                 .add("xsd", "http://www.w3.org/2001/XMLSchema");
 
-        from("activemq:queue:anshar.siri.router?asyncConsumer=true")
+        from("activemq:queue:" + ROUTER_QUEUE + "?asyncConsumer=true")
                 .to("log:router:" + getClass().getSimpleName() + "?showAll=true&multiline=true")
                 .choice()
                 .when().xpath("/siri:Siri/siri:HeartbeatNotification", ns)
-                    .to("activemq:queue:anshar.siri.process.heartbeat")
+                    .to("activemq:queue:" + HEARTBEAT_QUEUE)
                 .endChoice()
                 .when().xpath("/siri:Siri/siri:ServiceDelivery/siri:SituationExchangeDelivery", ns)
-                    .to("activemq:queue:anshar.siri.process.sx")
+                    .to("activemq:queue:" + SITUATION_EXCHANGE_QUEUE)
                  .endChoice()
                 .when().xpath("/siri:Siri/siri:ServiceDelivery/siri:VehicleMonitoringDelivery", ns)
-                    .to("activemq:queue:anshar.siri.process.vm")
+                    .to("activemq:queue:" + VEHICLE_MONITORING_QUEUE)
                 .endChoice()
                 .when().xpath("/siri:Siri/siri:ServiceDelivery/siri:EstimatedTimetableDelivery", ns)
-                    .to("activemq:queue:anshar.siri.process.et")
+                    .to("activemq:queue:" + ESTIMATED_TIMETABLE_QUEUE)
                 .endChoice()
                 .otherwise()
-                    .to("activemq:queue:anshar.siri.process")
+                    .to("activemq:queue:" + DEFAULT_PROCESSOR_QUEUE)
                 .end()
         ;
 
 
-        from("activemq:queue:anshar.siri.process?asyncConsumer=true")
+        from("activemq:queue:" + DEFAULT_PROCESSOR_QUEUE + "?asyncConsumer=true")
                 .to("log:processor:" + getClass().getSimpleName() + "?showAll=true&multiline=true")
                 .process(p -> {
                     String subscriptionId = getSubscriptionIdFromPath(p.getIn().getHeader("CamelHttpPath", String.class));
@@ -82,7 +106,17 @@ public class SiriIncomingReceiver extends RouteBuilder {
 
                 });
 
-        from("activemq:queue:anshar.siri.process.sx?asyncConsumer=true")
+        from("activemq:queue:" + HEARTBEAT_QUEUE + "?asyncConsumer=true")
+                .to("log:heartbeat:" + getClass().getSimpleName() + "?showAll=true&multiline=true")
+                .process(p -> {
+                    String subscriptionId = getSubscriptionIdFromPath(p.getIn().getHeader("CamelHttpPath", String.class));
+
+                    String xml = p.getIn().getBody(String.class);
+                    SiriHandler.handleIncomingSiri(subscriptionId, xml);
+
+                });
+
+        from("activemq:queue:" + SITUATION_EXCHANGE_QUEUE + "?asyncConsumer=true")
                 .to("log:processor-SX:" + getClass().getSimpleName() + "?showAll=true&multiline=true")
                 .process(p -> {
                     String subscriptionId = getSubscriptionIdFromPath(p.getIn().getHeader("CamelHttpPath", String.class));
@@ -92,7 +126,7 @@ public class SiriIncomingReceiver extends RouteBuilder {
 
                 });
 
-        from("activemq:queue:anshar.siri.process.vm?asyncConsumer=true")
+        from("activemq:queue:" + VEHICLE_MONITORING_QUEUE + "?asyncConsumer=true")
                 .to("log:processor-VM:" + getClass().getSimpleName() + "?showAll=true&multiline=true")
                 .process(p -> {
 
@@ -103,7 +137,7 @@ public class SiriIncomingReceiver extends RouteBuilder {
 
                 });
 
-        from("activemq:queue:anshar.siri.process.et?asyncConsumer=true")
+        from("activemq:queue:" + ESTIMATED_TIMETABLE_QUEUE + "?asyncConsumer=true")
                 .to("log:processor-ET:" + getClass().getSimpleName() + "?showAll=true&multiline=true")
                 .process(p -> {
                     String subscriptionId = getSubscriptionIdFromPath(p.getIn().getHeader("CamelHttpPath", String.class));
