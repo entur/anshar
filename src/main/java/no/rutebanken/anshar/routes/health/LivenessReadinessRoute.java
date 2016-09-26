93e784e76af1a6ffe4d005b309c6a9f630fd0392
Lasse Tyrihjell <lassetyr@gmail.com>
2016-09-26 10:23:41 +0200

    Added simple hazelcast-check to health-check

--- src/main/java/no/rutebanken/anshar/routes/health/LivenessReadinessRoute.java
+++ src/main/java/no/rutebanken/anshar/routes/health/LivenessReadinessRoute.java
@@ -1,13 +1,19 @@
 package no.rutebanken.anshar.routes.health;
 
+import com.hazelcast.core.HazelcastInstanceNotActiveException;
+import no.rutebanken.anshar.messages.DistributedCollection;
 import no.rutebanken.anshar.routes.outbound.ServerSubscriptionManager;
 import no.rutebanken.anshar.subscription.SubscriptionManager;
+import no.rutebanken.anshar.subscription.SubscriptionSetup;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+import java.util.Set;
 
 
 @Configuration
@@ -31,8 +37,22 @@ public class LivenessReadinessRoute extends RouteBuilder {
                 .setBody(constant("OK"))
         ;
         from("jetty:http://0.0.0.0:" + inboundPort + "/up")
-                .setHeader(Exchange.HTTP_RESPONSE_CODE, constant("200"))
-                .setBody(constant("OK"))
+                .process(p -> {
+                    boolean healthy = true;
+                    try {
+                        Map<String, SubscriptionSetup> activeSubscriptionsMap = DistributedCollection.getActiveSubscriptionsMap();
+                        Set<String> keys = activeSubscriptionsMap.keySet();
+                    } catch (HazelcastInstanceNotActiveException hz) {
+                        healthy = false;
+                    }
+                    if (healthy) {
+                        p.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, constant("200"));
+                        p.getOut().setBody(constant("OK"));
+                    } else {
+                        p.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, constant("500"));
+                        p.getOut().setBody(constant("Hazelcast is down - caused by OutOfMemoryError?"));
+                    }
+                })
         ;
 
         //Return subscription status
