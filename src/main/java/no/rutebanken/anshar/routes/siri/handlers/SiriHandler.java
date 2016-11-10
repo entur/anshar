f0a2e970b850837b8b67527ac3b9c60026d7eae5
Lasse Tyrihjell <lassetyr@gmail.com>
2016-11-10 10:39:06 +0100

    Handling incoming deliveries with description of errors

--- src/main/java/no/rutebanken/anshar/routes/siri/handlers/SiriHandler.java
+++ src/main/java/no/rutebanken/anshar/routes/siri/handlers/SiriHandler.java
@@ -4,11 +4,13 @@ import no.rutebanken.anshar.messages.EstimatedTimetables;
 import no.rutebanken.anshar.messages.ProductionTimetables;
 import no.rutebanken.anshar.messages.Situations;
 import no.rutebanken.anshar.messages.VehicleActivities;
+import no.rutebanken.anshar.routes.ServiceNotSupportedException;
 import no.rutebanken.anshar.routes.outbound.ServerSubscriptionManager;
 import no.rutebanken.anshar.routes.siri.SiriObjectFactory;
 import no.rutebanken.anshar.routes.siri.transformer.SiriValueTransformer;
 import no.rutebanken.anshar.subscription.SubscriptionManager;
 import no.rutebanken.anshar.subscription.SubscriptionSetup;
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.org.siri.siri20.*;
@@ -16,7 +18,9 @@ import uk.org.siri.siri20.*;
 import javax.xml.bind.JAXBException;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SiriHandler {
 
@@ -137,13 +141,18 @@ public class SiriHandler {
                     logger.info("Got SX-delivery: Subscription [{}]", subscriptionSetup);
 
                     List<PtSituationElement> addedOrUpdated = new ArrayList<>();
-                    situationExchangeDeliveries.forEach(sx ->
+                    situationExchangeDeliveries.forEach(sx -> {
+                                if (sx.isStatus() != null && !sx.isStatus()) {
+                                    logger.info(getErrorContents(sx.getErrorCondition()));
+                                } else {
                                     sx.getSituations().getPtSituationElements().forEach(ptSx -> {
                                         PtSituationElement situationElement = Situations.add(ptSx, subscriptionSetup.getDatasetId());
                                         if (situationElement != null) {
                                             addedOrUpdated.add(situationElement);
                                         }
-                                    })
+                                    });
+                                }
+                            }
                     );
 
                     serverSubscriptionManager.pushUpdatedSituations(addedOrUpdated, subscriptionSetup.getDatasetId());
@@ -155,14 +164,19 @@ public class SiriHandler {
                     logger.info("Got VM-delivery: Subscription [{}]", subscriptionSetup);
 
                     List<VehicleActivityStructure> addedOrUpdated = new ArrayList<>();
-                    vehicleMonitoringDeliveries.forEach(vm ->
-                                    vm.getVehicleActivities().forEach(activity -> {
-                                                VehicleActivityStructure addedOrUpdatedActivity = VehicleActivities.add(activity, subscriptionSetup.getDatasetId());
-                                                if (addedOrUpdatedActivity != null) {
-                                                    addedOrUpdated.add(addedOrUpdatedActivity);
-                                                }
+                    vehicleMonitoringDeliveries.forEach(vm -> {
+                            if (vm.isStatus() != null && !vm.isStatus()) {
+                                logger.info(getErrorContents(vm.getErrorCondition()));
+                            } else {
+                                vm.getVehicleActivities().forEach(activity -> {
+                                            VehicleActivityStructure addedOrUpdatedActivity = VehicleActivities.add(activity, subscriptionSetup.getDatasetId());
+                                            if (addedOrUpdatedActivity != null) {
+                                                addedOrUpdated.add(addedOrUpdatedActivity);
                                             }
-                                    )
+                                        }
+                                );
+                            }
+                        }
                     );
 
                     serverSubscriptionManager.pushUpdatedVehicleActivities(addedOrUpdated, subscriptionSetup.getDatasetId());
@@ -175,14 +189,18 @@ public class SiriHandler {
 
                     List<EstimatedVehicleJourney> addedOrUpdated = new ArrayList<>();
                     estimatedTimetableDeliveries.forEach(et -> {
-                                et.getEstimatedJourneyVersionFrames().forEach(versionFrame -> {
-                                    versionFrame.getEstimatedVehicleJourneies().forEach(journey -> {
-                                        EstimatedVehicleJourney element = EstimatedTimetables.add(journey, subscriptionSetup.getDatasetId());
-                                        if (element != null) {
-                                            addedOrUpdated.add(journey);
-                                        }
+                                if (et.isStatus() != null && !et.isStatus()) {
+                                    logger.info(getErrorContents(et.getErrorCondition()));
+                                } else {
+                                    et.getEstimatedJourneyVersionFrames().forEach(versionFrame -> {
+                                        versionFrame.getEstimatedVehicleJourneies().forEach(journey -> {
+                                            EstimatedVehicleJourney element = EstimatedTimetables.add(journey, subscriptionSetup.getDatasetId());
+                                            if (element != null) {
+                                                addedOrUpdated.add(journey);
+                                            }
+                                        });
                                     });
-                                });
+                                }
                             }
                     );
                     serverSubscriptionManager.pushUpdatedEstimatedTimetables(addedOrUpdated, subscriptionSetup.getDatasetId());
@@ -194,9 +212,13 @@ public class SiriHandler {
 
                     List<ProductionTimetableDeliveryStructure> addedOrUpdated = new ArrayList<>();
                     productionTimetableDeliveries.forEach(pt -> {
-                                ProductionTimetableDeliveryStructure element = ProductionTimetables.add(pt, subscriptionSetup.getDatasetId());
-                                if (element != null) {
-                                    addedOrUpdated.add(element);
+                                if (pt.isStatus() != null && !pt.isStatus()) {
+                                    logger.info(getErrorContents(pt.getErrorCondition()));
+                                } else {
+                                    ProductionTimetableDeliveryStructure element = ProductionTimetables.add(pt, subscriptionSetup.getDatasetId());
+                                    if (element != null) {
+                                        addedOrUpdated.add(element);
+                                    }
                                 }
                             }
                     );
@@ -204,10 +226,73 @@ public class SiriHandler {
                     logger.info("Active PT-elements: {}, new/updated: {}", ProductionTimetables.getAll().size(), addedOrUpdated.size());
                 }
             } else {
-                return incoming;
+                throw new RuntimeException(new ServiceNotSupportedException());
             }
         } else {
             logger.debug("ServiceDelivery for invalid subscriptionId [{}] ignored.", subscriptionId);
+        }
+        return null;
+    }
+
+    /**
+     * Creates a json-string containing all potential errormessage-values
+     *
+     * @param errorCondition
+     * @return
+     */
+    private String getErrorContents(ServiceDeliveryErrorConditionElement errorCondition) {
+        String errorContents = "";
+        if (errorCondition != null) {
+            Map<String, String> errorMap = new HashMap<>();
+            String accessNotAllowed = getErrorText(errorCondition.getAccessNotAllowedError());
+            String allowedResourceUsageExceeded = getErrorText(errorCondition.getAllowedResourceUsageExceededError());
+            String beyondDataHorizon = getErrorText(errorCondition.getBeyondDataHorizon());
+            String capabilityNotSupportedError = getErrorText(errorCondition.getCapabilityNotSupportedError());
+            String endpointDeniedAccessError = getErrorText(errorCondition.getEndpointDeniedAccessError());
+            String endpointNotAvailableAccessError = getErrorText(errorCondition.getEndpointNotAvailableAccessError());
+            String invalidDataReferencesError = getErrorText(errorCondition.getInvalidDataReferencesError());
+            String parametersIgnoredError = getErrorText(errorCondition.getParametersIgnoredError());
+            String serviceNotAvailableError = getErrorText(errorCondition.getServiceNotAvailableError());
+            String unapprovedKeyAccessError = getErrorText(errorCondition.getUnapprovedKeyAccessError());
+            String unknownEndpointError = getErrorText(errorCondition.getUnknownEndpointError());
+            String unknownExtensionsError = getErrorText(errorCondition.getUnknownExtensionsError());
+            String unknownParticipantError = getErrorText(errorCondition.getUnknownParticipantError());
+            String noInfoForTopicError = getErrorText(errorCondition.getNoInfoForTopicError());
+            String otherError = getErrorText(errorCondition.getOtherError());
+
+            String description = getDescriptionText(errorCondition.getDescription());
+
+            if (accessNotAllowed != null) {errorMap.put("accessNotAllowed", accessNotAllowed);}
+            if (allowedResourceUsageExceeded != null) {errorMap.put("allowedResourceUsageExceeded", allowedResourceUsageExceeded);}
+            if (beyondDataHorizon != null) {errorMap.put("beyondDataHorizon", beyondDataHorizon);}
+            if (capabilityNotSupportedError != null) {errorMap.put("capabilityNotSupportedError", capabilityNotSupportedError);}
+            if (endpointDeniedAccessError != null) {errorMap.put("endpointDeniedAccessError", endpointDeniedAccessError);}
+            if (endpointNotAvailableAccessError != null) {errorMap.put("endpointNotAvailableAccessError", endpointNotAvailableAccessError);}
+            if (invalidDataReferencesError != null) {errorMap.put("invalidDataReferencesError", invalidDataReferencesError);}
+            if (parametersIgnoredError != null) {errorMap.put("parametersIgnoredError", parametersIgnoredError);}
+            if (serviceNotAvailableError != null) {errorMap.put("serviceNotAvailableError", serviceNotAvailableError);}
+            if (unapprovedKeyAccessError != null) {errorMap.put("unapprovedKeyAccessError", unapprovedKeyAccessError);}
+            if (unknownEndpointError != null) {errorMap.put("unknownEndpointError", unknownEndpointError);}
+            if (unknownExtensionsError != null) {errorMap.put("unknownExtensionsError", unknownExtensionsError);}
+            if (unknownParticipantError != null) {errorMap.put("unknownParticipantError", unknownParticipantError);}
+            if (noInfoForTopicError != null) {errorMap.put("noInfoForTopicError", noInfoForTopicError);}
+            if (otherError != null) {errorMap.put("otherError", otherError);}
+            if (description != null) {errorMap.put("description", description);}
+
+            errorContents = JSONObject.toJSONString(errorMap);
+        }
+        return errorContents;
+    }
+
+    private String getErrorText(ErrorCodeStructure accessNotAllowedError) {
+        if (accessNotAllowedError != null) {
+            return accessNotAllowedError.getErrorText();
+        }
+        return null;
+    }
+    private String getDescriptionText(ErrorDescriptionStructure description) {
+        if (description != null) {
+            return description.getValue();
         }
         return null;
     }
---
+++ src/test/java/no/rutebanken/anshar/siri/handler/SiriHandlerTest.java
@@ -0,0 +1,171 @@
+package no.rutebanken.anshar.siri.handler;
+
+import no.rutebanken.anshar.routes.siri.handlers.SiriHandler;
+import no.rutebanken.anshar.subscription.SubscriptionManager;
+import no.rutebanken.anshar.subscription.SubscriptionSetup;
+import org.junit.Before;
+import org.junit.Test;
+import uk.org.siri.siri20.Siri;
+
+import javax.xml.bind.JAXBException;
+
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.fail;
+import static junit.framework.TestCase.assertNotNull;
+
+public class SiriHandlerTest {
+    private SiriHandler handler;
+
+    String SX_SUBSCRIPTION_ID = "1234";
+    String ET_SUBSCRIPTION_ID = "2345";
+    String VM_SUBSCRIPTION_ID = "3456";
+    String PT_SUBSCRIPTION_ID = "4567";
+
+    @Before
+    public void setUp() {
+        handler = new SiriHandler();
+
+        SubscriptionManager.addSubscription(SX_SUBSCRIPTION_ID, getSxSubscription());
+        SubscriptionManager.addSubscription(ET_SUBSCRIPTION_ID, getEtSubscription());
+        SubscriptionManager.addSubscription(VM_SUBSCRIPTION_ID, getVmSubscription());
+        SubscriptionManager.addSubscription(PT_SUBSCRIPTION_ID, getPtSubscription());
+
+    }
+
+    @Test
+    public void testErrorInSXServiceDelivery() throws JAXBException {
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<siri:Siri xmlns:siri=\"http://www.siri.org.uk/siri\">\n" +
+                "  <siril:ServiceDelivery xmlns:siril=\"http://www.siri.org.uk/siri\">\n" +
+                "    <ResponseTimestamp xmlns=\"http://www.siri.org.uk/siri\">2016-11-10T04:27:15.9028457+01:00</ResponseTimestamp>\n" +
+                "    <ProducerRef xmlns=\"http://www.siri.org.uk/siri\">ATB</ProducerRef>\n" +
+                "    <ResponseMessageIdentifier xmlns=\"http://www.siri.org.uk/siri\">R_</ResponseMessageIdentifier>\n" +
+                "    <SituationExchangeDelivery xmlns=\"http://www.siri.org.uk/siri\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"                                 xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"                                 xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\"                                 version=\"2.0\">\n" +
+                "      <ResponseTimestamp>2016-11-10T04:27:15.9028457+01:00</ResponseTimestamp>\n" +
+                "      <RequestMessageRef>e1995179-cc74-4354-84b2-dbb9850c1b9a</RequestMessageRef>\n" +
+                "      <Status>false</Status>\n" +
+                "      <ErrorCondition>\n" +
+                "        <NoInfoForTopicError/>\n" +
+                "        <Description>Unable to connect to the remote server</Description>\n" +
+                "      </ErrorCondition>\n" +
+                "    </SituationExchangeDelivery>\n" +
+                "  </siril:ServiceDelivery>\n" +
+                "</siri:Siri>\n";
+
+        try {
+            Siri siri = handler.handleIncomingSiri(SX_SUBSCRIPTION_ID, xml);
+        } catch (Throwable t) {
+            fail("Handling empty response caused exception");
+        }
+    }
+
+
+    @Test
+    public void testErrorInETServiceDelivery() throws JAXBException {
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<siri:Siri xmlns:siri=\"http://www.siri.org.uk/siri\">\n" +
+                "  <siril:ServiceDelivery xmlns:siril=\"http://www.siri.org.uk/siri\">\n" +
+                "    <ResponseTimestamp xmlns=\"http://www.siri.org.uk/siri\">2016-11-10T04:27:15.9028457+01:00</ResponseTimestamp>\n" +
+                "    <ProducerRef xmlns=\"http://www.siri.org.uk/siri\">ATB</ProducerRef>\n" +
+                "    <ResponseMessageIdentifier xmlns=\"http://www.siri.org.uk/siri\">R_</ResponseMessageIdentifier>\n" +
+                "    <EstimatedTimetableDelivery xmlns=\"http://www.siri.org.uk/siri\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"                                 xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"                                 xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\"                                 version=\"2.0\">\n" +
+                "      <ResponseTimestamp>2016-11-10T04:27:15.9028457+01:00</ResponseTimestamp>\n" +
+                "      <RequestMessageRef>e1995179-cc74-4354-84b2-dbb9850c1b9a</RequestMessageRef>\n" +
+                "      <Status>false</Status>\n" +
+                "      <ErrorCondition>\n" +
+                "        <NoInfoForTopicError/>\n" +
+                "        <Description>Unable to connect to the remote server</Description>\n" +
+                "      </ErrorCondition>\n" +
+                "    </EstimatedTimetableDelivery>\n" +
+                "  </siril:ServiceDelivery>\n" +
+                "</siri:Siri>\n";
+
+        try {
+            handler.handleIncomingSiri(ET_SUBSCRIPTION_ID, xml);
+        } catch (Throwable t) {
+            fail("Handling empty response caused exception");
+        }
+    }
+
+
+    @Test
+    public void testErrorInVMServiceDelivery() throws JAXBException {
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<siri:Siri xmlns:siri=\"http://www.siri.org.uk/siri\">\n" +
+                "  <siril:ServiceDelivery xmlns:siril=\"http://www.siri.org.uk/siri\">\n" +
+                "    <ResponseTimestamp xmlns=\"http://www.siri.org.uk/siri\">2016-11-10T04:27:15.9028457+01:00</ResponseTimestamp>\n" +
+                "    <ProducerRef xmlns=\"http://www.siri.org.uk/siri\">ATB</ProducerRef>\n" +
+                "    <ResponseMessageIdentifier xmlns=\"http://www.siri.org.uk/siri\">R_</ResponseMessageIdentifier>\n" +
+                "    <VehicleMonitoringDelivery xmlns=\"http://www.siri.org.uk/siri\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"                                 xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"                                 xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\"                                 version=\"2.0\">\n" +
+                "      <ResponseTimestamp>2016-11-10T04:27:15.9028457+01:00</ResponseTimestamp>\n" +
+                "      <RequestMessageRef>e1995179-cc74-4354-84b2-dbb9850c1b9a</RequestMessageRef>\n" +
+                "      <Status>false</Status>\n" +
+                "      <ErrorCondition>\n" +
+                "        <NoInfoForTopicError/>\n" +
+                "        <Description>Unable to connect to the remote server</Description>\n" +
+                "      </ErrorCondition>\n" +
+                "    </VehicleMonitoringDelivery>\n" +
+                "  </siril:ServiceDelivery>\n" +
+                "</siri:Siri>\n";
+        try {
+            handler.handleIncomingSiri(VM_SUBSCRIPTION_ID, xml);
+        } catch (Throwable t) {
+            fail("Handling empty response caused exception");
+        }
+    }
+
+
+    @Test
+    public void testErrorInPTServiceDelivery() throws JAXBException {
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<siri:Siri xmlns:siri=\"http://www.siri.org.uk/siri\">\n" +
+                "  <siril:ServiceDelivery xmlns:siril=\"http://www.siri.org.uk/siri\">\n" +
+                "    <ResponseTimestamp xmlns=\"http://www.siri.org.uk/siri\">2016-11-10T04:27:15.9028457+01:00</ResponseTimestamp>\n" +
+                "    <ProducerRef xmlns=\"http://www.siri.org.uk/siri\">ATB</ProducerRef>\n" +
+                "    <ResponseMessageIdentifier xmlns=\"http://www.siri.org.uk/siri\">R_</ResponseMessageIdentifier>\n" +
+                "    <ProductionTimetableDelivery xmlns=\"http://www.siri.org.uk/siri\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"                                 xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"                                 xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\"                                 version=\"2.0\">\n" +
+                "      <ResponseTimestamp>2016-11-10T04:27:15.9028457+01:00</ResponseTimestamp>\n" +
+                "      <RequestMessageRef>e1995179-cc74-4354-84b2-dbb9850c1b9a</RequestMessageRef>\n" +
+                "      <Status>false</Status>\n" +
+                "      <ErrorCondition>\n" +
+                "        <NoInfoForTopicError/>\n" +
+                "        <Description>Unable to connect to the remote server</Description>\n" +
+                "      </ErrorCondition>\n" +
+                "    </ProductionTimetableDelivery>\n" +
+                "  </siril:ServiceDelivery>\n" +
+                "</siri:Siri>\n";
+        try {
+            handler.handleIncomingSiri(PT_SUBSCRIPTION_ID, xml);
+        } catch (Throwable t) {
+            fail("Handling empty response caused exception");
+        }
+    }
+
+
+
+    private SubscriptionSetup getSxSubscription() {
+        SubscriptionSetup setup = new SubscriptionSetup();
+        setup.setSubscriptionType(SubscriptionSetup.SubscriptionType.SITUATION_EXCHANGE);
+        return setup;
+    }
+
+    private SubscriptionSetup getVmSubscription() {
+        SubscriptionSetup setup = new SubscriptionSetup();
+        setup.setSubscriptionType(SubscriptionSetup.SubscriptionType.VEHICLE_MONITORING);
+        return setup;
+    }
+
+    private SubscriptionSetup getEtSubscription() {
+        SubscriptionSetup setup = new SubscriptionSetup();
+        setup.setSubscriptionType(SubscriptionSetup.SubscriptionType.ESTIMATED_TIMETABLE);
+        return setup;
+    }
+
+    private SubscriptionSetup getPtSubscription() {
+        SubscriptionSetup setup = new SubscriptionSetup();
+        setup.setSubscriptionType(SubscriptionSetup.SubscriptionType.PRODUCTION_TIMETABLE);
+        return setup;
+    }
+
+
+}
