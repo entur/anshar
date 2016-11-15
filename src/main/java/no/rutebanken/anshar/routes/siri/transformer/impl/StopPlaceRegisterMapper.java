02a6f0b2830a05e77465ec67f566981e1a270b33
Lasse Tyrihjell <lassetyr@gmail.com>
2016-11-15 12:32:33 +0100

    Temporary solution - including id-mapping in application

--- src/main/java/no/rutebanken/anshar/routes/siri/transformer/impl/StopPlaceRegisterMapper.java
+++ src/main/java/no/rutebanken/anshar/routes/siri/transformer/impl/StopPlaceRegisterMapper.java
@@ -11,9 +11,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
@@ -90,9 +90,14 @@ public class StopPlaceRegisterMapper extends ValueAdapter {
                 // Results in only one attempt during startup
 
                 attemptedToInitialize = true;
-                logger.info("Initializing data - start. Fetching mapping-data from {}", stopPlaceMappingUrl);
-                URL url = new URL(stopPlaceMappingUrl);
-                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+//                logger.info("Initializing data - start. Fetching mapping-data from {}", stopPlaceMappingUrl);
+//                URL url = new URL(stopPlaceMappingUrl);
+//                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+                logger.info("Initializing data - start. Fetching mapping-data from internal file ");
+                ClassLoader classLoader = getClass().getClassLoader();
+                File file = new File(classLoader.getResource("id_mapping.csv").getFile());
+                BufferedReader in = new BufferedReader(new FileReader(file));
 
                 String inputLine;
                 int duplicates = 0;
