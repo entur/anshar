08469e6816d69a7d95a6e08cf71997cdbbd3d202
Morten Friesgaard <friesgaard@gmail.com>
2017-05-11 13:51:46 +0200

    adds a message test

--- src/test/java/no/rutebanken/anshar/routes/mqtt/SiriVmMqttRouteTest.java
+++ src/test/java/no/rutebanken/anshar/routes/mqtt/SiriVmMqttRouteTest.java
@@ -1,6 +1,7 @@
 package no.rutebanken.anshar.routes.mqtt;
 
 import javafx.util.Pair;
+import org.json.JSONObject;
 import org.junit.Test;
 import uk.org.siri.siri20.*;
 
@@ -16,23 +17,50 @@ import static org.junit.Assert.*;
 
 public class SiriVmMqttRouteTest {
 
-    //
-    //
-
     @Test
     public void testTopicFormat() {
 
-        ZonedDateTime dateTime = ZonedDateTime.of(2017, 12, 24, 10, 23, 4, 0, ZoneId.of("GMT"));
+        ZonedDateTime dateTime = ZonedDateTime.of(2017, 12, 24, 10, 11, 4, 0, ZoneId.of("GMT"));
 
         VehicleActivityStructure vehicle = createVehicle(dateTime, "veh123", "RUT:Line:7890", "Nettbus",
-                1, "NSR:Quay:6201", 59.10234567, 10.98765421, 123, "Helsfyr T",
-                "7890", "Nydalen T", 18);
+                1, "NSR:Quay:4321", 59.10234567, 10.98765421, 123, "DesttNNx tehd",
+                "7890", "not used in this test", 1);
 
         String datasetId = "RUT";
         Pair<String, String> message = new SiriVmMqttRoute().getMessage(datasetId, vehicle);
         String topic = message.getKey();
 
-        assertEquals("/hfp/journey/bus/RUTveh123/RUT:Line:7890/1/Helsfyr T/1023/NSR:Quay:6201/59;10/19/08/27/", topic);
+        assertEquals("/hfp/journey/bus/RUTveh123/RUT:Line:7890/1/DesttNNx tehd/1011/NSR:Quay:4321/59;10/19/08/27/", topic);
+    }
+
+    @Test
+    public void testMessage() {
+        ZonedDateTime dateTime = ZonedDateTime.of(2017, 12, 24, 9, 37, 4, 0, ZoneId.of("GMT"));
+        VehicleActivityStructure vehicle = createVehicle(dateTime, "78123", "RUT:Line:0037", "Nobina",
+                2, "NSR:Quay:6201", 59.10234566, 10.98765422, 203, "Helsfyr T",
+                "37", "Nydalen T", 18);
+
+        String datasetId = "RUT";
+        Pair<String, String> message = new SiriVmMqttRoute().getMessage(datasetId, vehicle);
+        String msg = message.getValue();
+
+        JSONObject obj = new JSONObject(msg).getJSONObject(VehiclePosition.ROOT);
+
+        assertEquals("37", obj.get(VehiclePosition.DESIGNATION));
+        assertEquals("2", obj.get(VehiclePosition.DIRECTION));
+        assertEquals("Nobina", obj.get(VehiclePosition.OPERATOR));
+        assertEquals("RUT78123", obj.get(VehiclePosition.VEHICLE_ID));
+        assertEquals("2017-12-24T09:37:04Z", obj.get(VehiclePosition.TIMESTAMP));
+        assertEquals(1514108224, obj.get(VehiclePosition.TSI));
+        assertEquals(59.10234566, obj.get(VehiclePosition.LATITUDE));
+        assertEquals(10.98765422, obj.get(VehiclePosition.LONGITUDE));
+        assertEquals(203, obj.get(VehiclePosition.DELAY));
+        assertEquals("2017-12-24", obj.get(VehiclePosition.ODAY));
+        assertEquals("Nydalen T -> Helsfyr T", obj.get(VehiclePosition.JOURNEY));
+        assertEquals("RUT:Line:0037", obj.get(VehiclePosition.LINE));
+        assertEquals("0937", obj.get(VehiclePosition.STARTTIME));
+        assertEquals(18, obj.get(VehiclePosition.STOP_INDEX));
+        assertEquals("rutebanken", obj.get(VehiclePosition.SOURCE));
     }
 
     private VehicleActivityStructure createVehicle(ZonedDateTime dateTime, String vehicleRef, String line,
@@ -64,7 +92,7 @@ public class SiriVmMqttRouteTest {
         directionRef.setValue("" + direction);
         vehicleJourney.setDirectionRef(directionRef);
 
-        vehicleJourney.setOriginAimedDepartureTime(dateTime.minusMinutes(23));
+        vehicleJourney.setOriginAimedDepartureTime(dateTime);
 
         StopPointRef stopPointRef = new StopPointRef();
         stopPointRef.setValue(nextStop);
