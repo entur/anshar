2f670124b6c01c903b7ed5e846449d3b104b9f6e
Lasse Tyrihjell <lassetyr@gmail.com>
2017-06-08 08:27:52 +0200

    Clean up inbound URL in setter - it is used in equals()

--- src/main/java/no/rutebanken/anshar/subscription/SubscriptionSetup.java
+++ src/main/java/no/rutebanken/anshar/subscription/SubscriptionSetup.java
@@ -89,9 +89,6 @@ public class SubscriptionSetup implements Serializable {
     }
 
     public String buildUrl(boolean includeServerAddress) {
-        if (address.endsWith("/")) {
-            address = address.substring(0, address.length()-1);
-        }
         return (includeServerAddress ? address:"") + MessageFormat.format("/{0}/{1}/{2}/{3}", version, serviceType == ServiceType.REST ? "rs" : "ws", vendor, subscriptionId);
     }
 
@@ -327,6 +324,9 @@ public class SubscriptionSetup implements Serializable {
     }
 
     public void setAddress(String address) {
+        if (address.endsWith("/")) {
+            address = address.substring(0, address.length()-1);
+        }
         this.address = address;
     }
 
