a4810b1f73bad93992639d77ca66e88b942f2e67
Lasse Tyrihjell <lassetyr@gmail.com>
2017-08-11 12:32:59 +0200

    NRP-1953: Upgrading fabric8

--- src/main/java/no/rutebanken/anshar/App.java
+++ src/main/java/no/rutebanken/anshar/App.java
@@ -1,17 +1,17 @@
 package no.rutebanken.anshar;
 
-import org.apache.camel.spring.boot.FatJarRouter;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * A spring-boot application that includes a Camel route builder to setup the Camel routes
  */
 @SpringBootApplication
-public class App extends FatJarRouter {
+public class App {
 
     // must have a main method spring-boot can run
     public static void main(String[] args) {
-        FatJarRouter.main(args);
+        SpringApplication.run(App.class, args);
     }
 
 }
