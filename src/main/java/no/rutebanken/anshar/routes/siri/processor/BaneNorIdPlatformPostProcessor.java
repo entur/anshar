30bc36721b5c046a47426c902fcd81990f4c740e
Lasse Tyrihjell <lassetyr@gmail.com>
2017-08-30 09:32:35 +0200

    Using traditional for-loops for performance

--- src/main/java/no/rutebanken/anshar/routes/siri/processor/BaneNorIdPlatformPostProcessor.java
+++ src/main/java/no/rutebanken/anshar/routes/siri/processor/BaneNorIdPlatformPostProcessor.java
@@ -5,78 +5,75 @@ import no.rutebanken.anshar.routes.siri.transformer.SiriValueTransformer;
 import no.rutebanken.anshar.routes.siri.transformer.ValueAdapter;
 import uk.org.siri.siri20.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class BaneNorIdPlatformPostProcessor extends ValueAdapter implements PostProcessor {
 
+    private static BaneNorIdPlatformUpdaterService stopPlaceService;
+
     public static String getNsrId(String jbvCode, String platform) {
 
-        BaneNorIdPlatformUpdaterService stopPlaceService = ApplicationContextHolder.getContext().getBean(BaneNorIdPlatformUpdaterService.class);
+        if (stopPlaceService == null) {
+            stopPlaceService = ApplicationContextHolder.getContext().getBean(BaneNorIdPlatformUpdaterService.class);
+        }
 
         return stopPlaceService.get(jbvCode + ":" + platform);
     }
 
     @Override
     public void process(Siri siri) {
-        List<String> notFound = new ArrayList<>();
         List<EstimatedTimetableDeliveryStructure> estimatedTimetableDeliveries = siri.getServiceDelivery().getEstimatedTimetableDeliveries();
         if (estimatedTimetableDeliveries != null) {
             for (EstimatedTimetableDeliveryStructure estimatedTimetableDelivery : estimatedTimetableDeliveries) {
                 List<EstimatedVersionFrameStructure> estimatedJourneyVersionFrames = estimatedTimetableDelivery.getEstimatedJourneyVersionFrames();
                 if (estimatedJourneyVersionFrames != null) {
-                    estimatedJourneyVersionFrames.stream()
-                            .forEach(estimatedVersionFrameStructure -> {
-                                estimatedVersionFrameStructure.getEstimatedVehicleJourneies().forEach(estimatedVehicleJourney -> {
-                                    EstimatedVehicleJourney.EstimatedCalls estimatedCalls = estimatedVehicleJourney.getEstimatedCalls();
-                                    if (estimatedCalls != null) {
-                                        estimatedCalls.getEstimatedCalls().stream()
-                                                .forEach(et -> {
-                                                    String stopPointRefValue = et.getStopPointRef().getValue();
+                    for (EstimatedVersionFrameStructure estimatedVersionFrameStructure : estimatedJourneyVersionFrames) {
+                        if (estimatedVersionFrameStructure != null) {
+                            for (EstimatedVehicleJourney estimatedVehicleJourney : estimatedVersionFrameStructure.getEstimatedVehicleJourneies()) {
+                                EstimatedVehicleJourney.EstimatedCalls estimatedCalls = estimatedVehicleJourney.getEstimatedCalls();
+                                if (estimatedCalls != null) {
+                                    for (EstimatedCall et : estimatedCalls.getEstimatedCalls()) {
+                                        String stopPointRefValue = et.getStopPointRef().getValue();
 
-                                                    String platform = null;
-                                                    if (et.getArrivalPlatformName() != null) {
-                                                        platform = et.getArrivalPlatformName().getValue();
-                                                    } else if (et.getDeparturePlatformName() != null) {
-                                                        platform = et.getDeparturePlatformName().getValue();
-                                                    }
-                                                    if (platform == null) {
-                                                        platform = "1";
-                                                    }
-                                                    String nsrId = getNsrId(stopPointRefValue, platform);
-                                                    if (nsrId != null) {
-                                                        StopPointRef stopPointRef = new StopPointRef();
-                                                        stopPointRef.setValue(stopPointRefValue + SiriValueTransformer.SEPARATOR + nsrId);
-                                                        et.setStopPointRef(stopPointRef);
-                                                    }
-                                                });
+                                        String platform = null;
+                                        if (et.getArrivalPlatformName() != null) {
+                                            platform = et.getArrivalPlatformName().getValue();
+                                        } else if (et.getDeparturePlatformName() != null) {
+                                            platform = et.getDeparturePlatformName().getValue();
+                                        }
+                                        if (platform == null) {
+                                            platform = "1";
+                                        }
+                                        String nsrId = getNsrId(stopPointRefValue, platform);
+                                        if (nsrId != null) {
+                                            et.getStopPointRef().setValue(stopPointRefValue + SiriValueTransformer.SEPARATOR + nsrId);
+                                        }
                                     }
+                                }
 
-                                    EstimatedVehicleJourney.RecordedCalls recordedCalls = estimatedVehicleJourney.getRecordedCalls();
-                                    if (recordedCalls != null) {
-                                        recordedCalls.getRecordedCalls().stream()
-                                                .forEach(rc -> {
-                                                    String stopPointRefValue = rc.getStopPointRef().getValue();
+                                EstimatedVehicleJourney.RecordedCalls recordedCalls = estimatedVehicleJourney.getRecordedCalls();
+                                if (recordedCalls != null) {
+                                    for (RecordedCall rc : recordedCalls.getRecordedCalls()) {
+                                        String stopPointRefValue = rc.getStopPointRef().getValue();
 
-                                                    String platform = null;
-                                                    if (rc.getArrivalPlatformName() != null) {
-                                                        platform = rc.getArrivalPlatformName().getValue();
-                                                    } else if (rc.getDeparturePlatformName() != null) {
-                                                        platform = rc.getDeparturePlatformName().getValue();
-                                                    }
-                                                    if (platform == null) {
-                                                        platform = "1";
-                                                    }
-                                                    StopPointRef stopPointRef = new StopPointRef();
-                                                    String nsrId = getNsrId(stopPointRefValue, platform);
-                                                    stopPointRef.setValue(stopPointRefValue + SiriValueTransformer.SEPARATOR + nsrId);
-                                                    if (nsrId != null) {
-                                                        rc.setStopPointRef(stopPointRef);
-                                                    }
-                                                });
+                                        String platform = null;
+                                        if (rc.getArrivalPlatformName() != null) {
+                                            platform = rc.getArrivalPlatformName().getValue();
+                                        } else if (rc.getDeparturePlatformName() != null) {
+                                            platform = rc.getDeparturePlatformName().getValue();
+                                        }
+                                        if (platform == null) {
+                                            platform = "1";
+                                        }
+                                        String nsrId = getNsrId(stopPointRefValue, platform);
+                                        if (nsrId != null) {
+                                            rc.getStopPointRef().setValue(stopPointRefValue + SiriValueTransformer.SEPARATOR + nsrId);
+                                        }
                                     }
-                                });
-                            });
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
