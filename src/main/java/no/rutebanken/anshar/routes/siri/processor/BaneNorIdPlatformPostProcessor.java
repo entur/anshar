ac59b5ef9e1c26b36a89117bc670bc7df74351ac
Lasse Tyrihjell <lassetyr@gmail.com>
2017-09-25 15:46:30 +0200

    Reduced logging uf unmapped station/platforms

--- src/main/java/no/rutebanken/anshar/routes/siri/processor/BaneNorIdPlatformPostProcessor.java
+++ src/main/java/no/rutebanken/anshar/routes/siri/processor/BaneNorIdPlatformPostProcessor.java
@@ -7,13 +7,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.org.siri.siri20.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BaneNorIdPlatformPostProcessor extends ValueAdapter implements PostProcessor {
 
     private Logger logger = LoggerFactory.getLogger(BaneNorIdPlatformPostProcessor.class);
 
     private static BaneNorIdPlatformUpdaterService stopPlaceService;
+
+    private static Set<String> unmappedJbvCodePlatform = new HashSet<>();
+    private static boolean listUpdated;
 
     public String getNsrId(String jbvCode, String platform) {
 
@@ -23,13 +28,14 @@ public class BaneNorIdPlatformPostProcessor extends ValueAdapter implements Post
 
         String nsrId = stopPlaceService.get(jbvCode + ":" + platform);
         if (nsrId == null) {
-            logger.warn("Unable to find mapped value for jbvCode/platform {}, {}", jbvCode, platform);
+            listUpdated = listUpdated | unmappedJbvCodePlatform.add(jbvCode + ":" + platform);
         }
         return nsrId;
     }
 
     @Override
     public void process(Siri siri) {
+        listUpdated = false;
         List<EstimatedTimetableDeliveryStructure> estimatedTimetableDeliveries = siri.getServiceDelivery().getEstimatedTimetableDeliveries();
         if (estimatedTimetableDeliveries != null) {
             for (EstimatedTimetableDeliveryStructure estimatedTimetableDelivery : estimatedTimetableDeliveries) {
@@ -81,6 +87,11 @@ public class BaneNorIdPlatformPostProcessor extends ValueAdapter implements Post
                 }
             }
         }
+        if (listUpdated) {
+            logger.warn("Unable to find mapped value for jbvCode/platforms: {}", unmappedJbvCodePlatform);
+            listUpdated = false;
+        }
+
     }
 
     @Override
