c1f2f936549366e5bd8cfa02a44e8d35cae39734
Lasse Tyrihjell <lassetyr@gmail.com>
2017-10-19 12:30:33 +0200

    Added mapping-rules for brakar

---
+++ src/main/java/no/rutebanken/anshar/routes/siri/adapters/BrakarValueAdapters.java
@@ -0,0 +1,34 @@
+package no.rutebanken.anshar.routes.siri.adapters;
+
+import no.rutebanken.anshar.routes.siri.transformer.ValueAdapter;
+import no.rutebanken.anshar.routes.siri.transformer.impl.LeftPaddingAdapter;
+import no.rutebanken.anshar.subscription.SubscriptionSetup;
+import uk.org.ifopt.siri20.StopPlaceRef;
+import uk.org.siri.siri20.StopPointRef;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mapping(id="brakar")
+public class BrakarValueAdapters extends MappingAdapter {
+
+
+    @Override
+    public List<ValueAdapter> getValueAdapters(SubscriptionSetup subscriptionSetup) {
+
+        List<ValueAdapter> valueAdapters = new ArrayList<>();
+                valueAdapters.add(new LeftPaddingAdapter(StopPlaceRef.class, 10, '0'));
+                valueAdapters.add(new LeftPaddingAdapter(StopPointRef.class, 10, '0'));
+
+        valueAdapters.addAll(createNsrIdMappingAdapters(subscriptionSetup.getIdMappingPrefixes()));
+
+        if (subscriptionSetup.getDatasetId() != null && !subscriptionSetup.getDatasetId().isEmpty()) {
+            List<ValueAdapter> datasetPrefix = createIdPrefixAdapters(subscriptionSetup.getDatasetId());
+            if (!subscriptionSetup.getMappingAdapters().containsAll(datasetPrefix)) {
+                subscriptionSetup.getMappingAdapters().addAll(datasetPrefix);
+            }
+        }
+
+        return valueAdapters;
+    }
+}
