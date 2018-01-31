7288b5dc55ee4cd35e62ce8ddecbeee995a1da73
Lasse Tyrihjell <lassetyr@gmail.com>
2018-01-31 16:48:00 +0100

    Keeping implementations separate

--- src/main/java/no/rutebanken/anshar/routes/siri/adapters/AtbValueAdapters.java
+++ src/main/java/no/rutebanken/anshar/routes/siri/adapters/AtbValueAdapters.java
@@ -1,8 +1,11 @@
 package no.rutebanken.anshar.routes.siri.adapters;
 
 import no.rutebanken.anshar.routes.siri.transformer.ValueAdapter;
+import no.rutebanken.anshar.routes.siri.transformer.impl.LeftPaddingAdapter;
 import no.rutebanken.anshar.subscription.SubscriptionSetup;
+import uk.org.siri.siri20.LineRef;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Mapping(id="atb")
@@ -11,6 +14,18 @@ public class AtbValueAdapters extends AktValueAdapters {
 
     @Override
     public List<ValueAdapter> getValueAdapters(SubscriptionSetup subscriptionSetup) {
-        return super.getValueAdapters(subscriptionSetup);
+        List<ValueAdapter> valueAdapters = new ArrayList<>();
+        valueAdapters.add(new LeftPaddingAdapter(LineRef.class, 4, '0'));
+
+        valueAdapters.addAll(createNsrIdMappingAdapters(subscriptionSetup.getSubscriptionType(), subscriptionSetup.getDatasetId(), subscriptionSetup.getIdMappingPrefixes()));
+
+        if (subscriptionSetup.getDatasetId() != null && !subscriptionSetup.getDatasetId().isEmpty()) {
+            List<ValueAdapter> datasetPrefix = createIdPrefixAdapters(subscriptionSetup.getDatasetId());
+            if (!subscriptionSetup.getMappingAdapters().containsAll(datasetPrefix)) {
+                subscriptionSetup.getMappingAdapters().addAll(datasetPrefix);
+            }
+        }
+
+        return valueAdapters;
     }
 }
