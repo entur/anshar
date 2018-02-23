c2c3ca666a05782f4f98a58943124a3813658ccf
Lasse Tyrihjell <lassetyr@gmail.com>
2018-02-23 13:21:36 +0100

    Including all reportTypes for NSB

--- src/main/java/no/rutebanken/anshar/routes/siri/adapters/NsbSxValueAdapters.java
+++ src/main/java/no/rutebanken/anshar/routes/siri/adapters/NsbSxValueAdapters.java
@@ -1,6 +1,5 @@
 package no.rutebanken.anshar.routes.siri.adapters;
 
-import no.rutebanken.anshar.routes.siri.processor.ReportTypeFilterPostProcessor;
 import no.rutebanken.anshar.routes.siri.transformer.ValueAdapter;
 import no.rutebanken.anshar.routes.siri.transformer.impl.JbvCodeMapper;
 import no.rutebanken.anshar.subscription.SubscriptionSetup;
@@ -18,7 +17,7 @@ public class NsbSxValueAdapters extends MappingAdapter {
 
         List<ValueAdapter> valueAdapters = new ArrayList<>();
         valueAdapters.add(new JbvCodeMapper(subscriptionSetup.getSubscriptionType(), subscriptionSetup.getDatasetId(), StopPointRef.class));
-        valueAdapters.add(new ReportTypeFilterPostProcessor("incident"));
+//        valueAdapters.add(new ReportTypeFilterPostProcessor("incident"));
 
         return valueAdapters;
     }
