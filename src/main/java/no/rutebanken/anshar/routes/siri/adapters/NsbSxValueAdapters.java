c390a4adb35d1df53bd3ec2d58a3f45727683d2e
Lasse Tyrihjell <lassetyr@gmail.com>
2018-03-14 08:56:41 +0100

    Remove reportType-filter from NSB SX

--- src/main/java/no/rutebanken/anshar/routes/siri/adapters/NsbSxValueAdapters.java
+++ src/main/java/no/rutebanken/anshar/routes/siri/adapters/NsbSxValueAdapters.java
@@ -1,6 +1,5 @@
 package no.rutebanken.anshar.routes.siri.adapters;
 
-import no.rutebanken.anshar.routes.siri.processor.ReportTypeFilterPostProcessor;
 import no.rutebanken.anshar.routes.siri.transformer.ValueAdapter;
 import no.rutebanken.anshar.routes.siri.transformer.impl.JbvCodeMapper;
 import no.rutebanken.anshar.subscription.SubscriptionSetup;
@@ -18,7 +17,6 @@ public class NsbSxValueAdapters extends MappingAdapter {
 
         List<ValueAdapter> valueAdapters = new ArrayList<>();
         valueAdapters.add(new JbvCodeMapper(subscriptionSetup.getSubscriptionType(), subscriptionSetup.getDatasetId(), StopPointRef.class));
-        valueAdapters.add(new ReportTypeFilterPostProcessor("incident"));
 
         return valueAdapters;
     }
