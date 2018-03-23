b01c80350f9901a6e9d71f66993f924c478b991c
Lasse Tyrihjell <lassetyr@gmail.com>
2018-03-23 18:26:38 +0100

    Added temporary "hack" to Arrival- and DepartureStatus correctly based on Cancellations.

---
+++ src/main/java/no/rutebanken/anshar/routes/siri/processor/BaneNorArrivalDepartureCancellationProcessor.java
@@ -0,0 +1,102 @@
+package no.rutebanken.anshar.routes.siri.processor;
+
+import no.rutebanken.anshar.routes.siri.transformer.ValueAdapter;
+import uk.org.siri.siri20.*;
+
+import java.util.List;
+
+public class BaneNorArrivalDepartureCancellationProcessor extends ValueAdapter implements PostProcessor {
+
+    public BaneNorArrivalDepartureCancellationProcessor() {
+    }
+
+    @Override
+    public void process(Siri siri) {
+        if (siri != null && siri.getServiceDelivery() != null) {
+            processEtDeliveries(siri.getServiceDelivery().getEstimatedTimetableDeliveries());
+        }
+    }
+
+    private void processEtDeliveries(List<EstimatedTimetableDeliveryStructure> etDeliveries) {
+        for (EstimatedTimetableDeliveryStructure etDelivery : etDeliveries) {
+            List<EstimatedVersionFrameStructure> estimatedJourneyVersionFrames = etDelivery.getEstimatedJourneyVersionFrames();
+            for (EstimatedVersionFrameStructure estimatedJourneyVersionFrame : estimatedJourneyVersionFrames) {
+                List<EstimatedVehicleJourney> estimatedVehicleJourneies = estimatedJourneyVersionFrame.getEstimatedVehicleJourneies();
+                for (EstimatedVehicleJourney estimatedVehicleJourney : estimatedVehicleJourneies) {
+
+                    EstimatedVehicleJourney.RecordedCalls recordedCalls = estimatedVehicleJourney.getRecordedCalls();
+
+                    boolean wasPreviousCallCancelled = (estimatedVehicleJourney.isIsCompleteStopSequence() != null && estimatedVehicleJourney.isIsCompleteStopSequence());
+
+                    boolean hasRecordedCalls = false;
+
+                    if (recordedCalls != null && recordedCalls.getRecordedCalls() != null) {
+                        RecordedCall recordedCall = recordedCalls.getRecordedCalls().get(recordedCalls.getRecordedCalls().size()-1);
+                        //Only need to check last RecordedCall - if it exists
+                        wasPreviousCallCancelled = recordedCall.isCancellation() != null && recordedCall.isCancellation();
+                        hasRecordedCalls = true;
+                    }
+
+                    EstimatedVehicleJourney.EstimatedCalls estimatedCalls = estimatedVehicleJourney.getEstimatedCalls();
+
+                    if (estimatedCalls != null && estimatedCalls.getEstimatedCalls() != null) {
+                        List<EstimatedCall> calls = estimatedCalls.getEstimatedCalls();
+                        for (int i = 0; i < calls.size()-1; i++) {
+                            EstimatedCall call = calls.get(i);
+                            EstimatedCall nextCall = calls.get(i+1);
+
+                            boolean isCallCancelled = call.isCancellation() != null && call.isCancellation();
+                            boolean isNextCallCancelled = nextCall.isCancellation() != null && nextCall.isCancellation();
+
+                            if (isCallCancelled) {
+                                if (!isNextCallCancelled && !hasRecordedCalls  && i == 0) {
+                                    // Only first stop is cancelled
+                                    // Cancellation is true, departure from current is cancelled, arrival on next is cancelled
+
+                                    call.setDepartureStatus(CallStatusEnumeration.CANCELLED);
+                                    call.setCancellation(true);
+                                    nextCall.setArrivalStatus(CallStatusEnumeration.CANCELLED);
+
+                                } else if (!wasPreviousCallCancelled && !isNextCallCancelled) {
+                                    // Single stop cancellation - arrival AND departure is cancelled
+                                    call.setArrivalStatus(CallStatusEnumeration.CANCELLED);
+                                    call.setDepartureStatus(CallStatusEnumeration.CANCELLED);
+                                    call.setCancellation(true);
+                                } else {
+                                    // Multiple stops are cancelled - arrival/departure should be set accordingly
+                                    if (wasPreviousCallCancelled) {
+                                        call.setArrivalStatus(CallStatusEnumeration.CANCELLED);
+                                    } else {
+                                        call.setCancellation(false);
+                                        // Setting ArrivalStatus to onTime - may be wrong, but currently, value is not used
+                                        call.setArrivalStatus(CallStatusEnumeration.ON_TIME);
+                                    }
+
+                                    if (isNextCallCancelled) {
+                                        call.setDepartureStatus(CallStatusEnumeration.CANCELLED);
+                                    } else {
+                                        call.setCancellation(false);
+                                        // Setting DepartureStatus to onTime - may be wrong, but currently, value is not used
+                                        call.setDepartureStatus(CallStatusEnumeration.ON_TIME);
+                                    }
+                                }
+                            } else if (isNextCallCancelled && i == calls.size()-2) {
+                                //Only last (next) stop is cancelled. Cancel departure from current
+                                call.setDepartureStatus(CallStatusEnumeration.CANCELLED);
+                            }
+
+                            wasPreviousCallCancelled = isCallCancelled;
+                        }
+                    }
+
+                }
+            }
+        }
+
+    }
+
+    @Override
+    protected String apply(String value) {
+        return null;
+    }
+}
---
+++ src/test/java/no/rutebanken/anshar/siri/processor/BaneNorArrivalDepartureCancellationProcessorTest.java
@@ -0,0 +1,253 @@
+package no.rutebanken.anshar.siri.processor;
+
+import no.rutebanken.anshar.routes.siri.helpers.SiriObjectFactory;
+import no.rutebanken.anshar.routes.siri.processor.BaneNorArrivalDepartureCancellationProcessor;
+import org.junit.Test;
+import uk.org.siri.siri20.*;
+
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class BaneNorArrivalDepartureCancellationProcessorTest {
+
+    BaneNorArrivalDepartureCancellationProcessor processor = new BaneNorArrivalDepartureCancellationProcessor();
+
+    @Test
+    public void testCancellationOnStart() {
+        boolean[] cancellations =                                   {true,         true,          false,          false};
+        boolean[][] expectedArrivalBoardingCancellationStatus = {{true, true}, {true, false}, {false, false}, {false, false}};
+
+        assertEquals("Wrong number of comparing patterns", cancellations.length, expectedArrivalBoardingCancellationStatus.length);
+
+        Siri siri = createEstimatedVehicleJourney(cancellations);
+
+        processor.process(siri);
+
+
+        verifyCancellations(siri, new boolean[0], cancellations, expectedArrivalBoardingCancellationStatus);
+    }
+
+    @Test
+    public void testCancellationOnEnd() {
+        boolean[] cancellations =                                   {false,          false,           false,         false,          true,         true,        true};
+        boolean[][] expectedArrivalBoardingCancellationStatus = {{false, false}, {false, false}, {false, false}, {false, false}, {false, true}, {true, true}, {true, true}};
+
+        assertEquals("Wrong number of comparing patterns", cancellations.length, expectedArrivalBoardingCancellationStatus.length);
+
+        Siri siri = createEstimatedVehicleJourney(cancellations);
+
+        processor.process(siri);
+
+
+        verifyCancellations(siri, new boolean[0], cancellations, expectedArrivalBoardingCancellationStatus);
+    }
+
+
+    @Test
+    public void testCancellationOnMiddle() {
+        boolean[] cancellations =                                   {false,          false,          true,          true,          false,          false,         false};
+        boolean[][] expectedArrivalBoardingCancellationStatus = {{false, false}, {false, false}, {false, true}, {true, false}, {false, false}, {false, false}, {false, false}};
+
+        assertEquals("Wrong number of comparing patterns", cancellations.length, expectedArrivalBoardingCancellationStatus.length);
+
+        Siri siri = createEstimatedVehicleJourney(cancellations);
+
+        processor.process(siri);
+
+
+        verifyCancellations(siri, new boolean[0], cancellations, expectedArrivalBoardingCancellationStatus);
+    }
+
+
+    @Test
+    public void testMultipleCancellationOnMiddle() {
+        boolean[] cancellations =                              {     false,          false,          true,         true,         false,           true,           true,           true,           false,       true,           true,         false,        false};
+        boolean[][] expectedArrivalBoardingCancellationStatus = {{false, false}, {false, false}, {false, true}, {true, false}, {false, false}, {false, true}, {true, true}, {true, false}, {false, false}, {false, true}, {true, false}, {false, false}, {false, false}};
+
+        assertEquals("Wrong number of comparing patterns", cancellations.length, expectedArrivalBoardingCancellationStatus.length);
+
+        Siri siri = createEstimatedVehicleJourney(cancellations);
+
+        processor.process(siri);
+
+
+        verifyCancellations(siri, new boolean[0], cancellations, expectedArrivalBoardingCancellationStatus);
+    }
+
+    @Test
+    public void testCancellationOnSingleStop() {
+        boolean[] cancellations =                                   {false,          false,         true,           false,         false,          false,         false};
+        boolean[][] expectedArrivalBoardingCancellationStatus = {{false, false}, {false, false}, {true, true}, {false, false}, {false, false}, {false, false}, {false, false}};
+
+        assertEquals("Wrong number of comparing patterns", cancellations.length, expectedArrivalBoardingCancellationStatus.length);
+
+        Siri siri = createEstimatedVehicleJourney(cancellations);
+
+        processor.process(siri);
+
+
+        verifyCancellations(siri, new boolean[0], cancellations, expectedArrivalBoardingCancellationStatus);
+    }
+
+
+    @Test
+    public void testCancellationOnOnlyFirstStop() {
+        boolean[] cancellations =                                   {true,        false,          false,          false,          false,           false,         false};
+        boolean[][] expectedArrivalBoardingCancellationStatus = {{true, true}, {true, false}, {false, false}, {false, false}, {false, false}, {false, false}, {false, false}};
+
+        assertEquals("Wrong number of comparing patterns", cancellations.length, expectedArrivalBoardingCancellationStatus.length);
+
+        Siri siri = createEstimatedVehicleJourney(cancellations);
+
+        processor.process(siri);
+
+
+        verifyCancellations(siri, new boolean[0], cancellations, expectedArrivalBoardingCancellationStatus);
+    }
+
+    @Test
+    public void testCancellationOnOnlyLastStop() {
+        boolean[] cancellations =                                   {false,          false,          false,           false,         false,          false,        true};
+        boolean[][] expectedArrivalBoardingCancellationStatus = {{false, false}, {false, false}, {false, false}, {false, false}, {false, false}, {false, true}, {true, true}};
+
+        assertEquals("Wrong number of comparing patterns", cancellations.length, expectedArrivalBoardingCancellationStatus.length);
+
+        Siri siri = createEstimatedVehicleJourney(cancellations);
+
+        processor.process(siri);
+
+
+        verifyCancellations(siri, new boolean[0], cancellations, expectedArrivalBoardingCancellationStatus);
+    }
+
+    @Test
+    public void testCancellationOnStartAndEnd() {
+        boolean[] cancellations =                                   {true,        true,          true,          false,          false,         false,          false,            true,         true,         true};
+        boolean[][] expectedArrivalBoardingCancellationStatus = {{true, true}, {true, true}, {true, false}, {false, false}, {false, false}, {false, false}, {false, false}, {false, true}, {true, true}, {true, true}};
+
+        assertEquals("Wrong number of comparing patterns", cancellations.length, expectedArrivalBoardingCancellationStatus.length);
+
+        Siri siri = createEstimatedVehicleJourney(cancellations);
+
+        processor.process(siri);
+
+        verifyCancellations(siri, new boolean[0], cancellations, expectedArrivalBoardingCancellationStatus);
+    }
+
+    @Test
+    public void testCancellationOnStartAfterRecordedCalls() {
+        boolean[] cancellations =                                   {true,        true,          true,           false,         false,          false,          false,           true,         true,         true};
+        boolean[][] expectedArrivalBoardingCancellationStatus = {{true, true}, {true, true}, {true, false}, {false, false}, {false, false}, {false, false}, {false, false}, {false, true}, {true, true}, {true, true}};
+
+        assertEquals("Wrong number of comparing patterns", cancellations.length, expectedArrivalBoardingCancellationStatus.length);
+
+        int recordedCallsCount = 2;
+
+        Siri siri = createEstimatedVehicleJourney(recordedCallsCount, cancellations);
+
+        processor.process(siri);
+
+
+        verifyCancellations(siri, Arrays.copyOfRange(cancellations, 0, recordedCallsCount), Arrays.copyOfRange(cancellations, recordedCallsCount, cancellations.length), expectedArrivalBoardingCancellationStatus);
+    }
+
+    private void verifyCancellations(Siri siri, boolean[] rcCancellations, boolean[] etCancellations, boolean[][] expectedArrivalBoardingCancellationStatus) {
+        EstimatedTimetableDeliveryStructure timetableDeliveryStructure = siri.getServiceDelivery().getEstimatedTimetableDeliveries().get(0);
+
+        List<EstimatedVehicleJourney> estimatedVehicleJourneies = timetableDeliveryStructure.getEstimatedJourneyVersionFrames().get(0).getEstimatedVehicleJourneies();
+        EstimatedVehicleJourney estimatedVehicleJourney = estimatedVehicleJourneies.get(0);
+        List<EstimatedCall> estimatedCalls = estimatedVehicleJourney.getEstimatedCalls().getEstimatedCalls();
+
+        List<RecordedCall> recordedCalls = new ArrayList<>();
+        if (estimatedVehicleJourney.getRecordedCalls() != null && estimatedVehicleJourney.getRecordedCalls().getRecordedCalls() != null) {
+            recordedCalls = estimatedVehicleJourney.getRecordedCalls().getRecordedCalls();
+        }
+
+        assertEquals(rcCancellations.length, recordedCalls.size());
+        assertEquals(etCancellations.length, estimatedCalls.size());
+
+        int numberOfCalls = rcCancellations.length + etCancellations.length;
+        assertEquals(numberOfCalls, expectedArrivalBoardingCancellationStatus.length);
+
+        int i = rcCancellations.length;
+        for (EstimatedCall estimatedCall : estimatedCalls) {
+
+            boolean isArrivalCancelled = expectedArrivalBoardingCancellationStatus[i][0];
+            boolean isDepartureCancelled = expectedArrivalBoardingCancellationStatus[i][1];
+
+            assertEquals("EstimatedCall does not match cancellation (" + i + ")", isArrivalCancelled & isDepartureCancelled, estimatedCall.isCancellation() != null && estimatedCall.isCancellation().booleanValue());
+            assertEquals("EstimatedCall does not match cancellation (" + i + ")", !isArrivalCancelled | !isDepartureCancelled, estimatedCall.isCancellation() == null | (estimatedCall.isCancellation() != null && !estimatedCall.isCancellation().booleanValue()));
+
+            assertEquals("EstimatedCall does not match on Arrival (" + i + ")", isArrivalCancelled, (estimatedCall.getArrivalStatus().equals(CallStatusEnumeration.CANCELLED)));
+            assertEquals("EstimatedCall does not match on Departure (" + i + ")", isDepartureCancelled, (estimatedCall.getDepartureStatus().equals(CallStatusEnumeration.CANCELLED)));
+            i++;
+        }
+
+    }
+
+
+    private Siri createEstimatedVehicleJourney(boolean... cancellationPattern) {
+        return createEstimatedVehicleJourney(0, cancellationPattern);
+    }
+    private Siri createEstimatedVehicleJourney(int recordedCallsCount, boolean... cancellationPattern) {
+        EstimatedVehicleJourney element = new EstimatedVehicleJourney();
+        LineRef lineRef = new LineRef();
+        lineRef.setValue("NSB:Line:1");
+        element.setLineRef(lineRef);
+        VehicleRef vehicleRef = new VehicleRef();
+        vehicleRef.setValue("2134");
+        element.setVehicleRef(vehicleRef);
+        element.setIsCompleteStopSequence(true);
+
+
+        EstimatedVehicleJourney.RecordedCalls recordedCalls = new EstimatedVehicleJourney.RecordedCalls();
+        for (int i = 0; i < recordedCallsCount; i++) {
+
+            StopPointRef stopPointRef = new StopPointRef();
+            stopPointRef.setValue("NSR:TEST:" + i);
+            RecordedCall call = new RecordedCall();
+            call.setStopPointRef(stopPointRef);
+
+            if (cancellationPattern[i]) {
+                call.setCancellation(cancellationPattern[i]);
+            }
+
+            recordedCalls.getRecordedCalls().add(call);
+        }
+        if (recordedCallsCount > 0) {
+            element.setRecordedCalls(recordedCalls);
+        }
+
+
+        EstimatedVehicleJourney.EstimatedCalls estimatedCalls = new EstimatedVehicleJourney.EstimatedCalls();
+        for (int i = recordedCallsCount; i < cancellationPattern.length; i++) {
+
+            StopPointRef stopPointRef = new StopPointRef();
+            stopPointRef.setValue("NSR:TEST:" + i);
+            EstimatedCall call = new EstimatedCall();
+            call.setStopPointRef(stopPointRef);
+
+            if (cancellationPattern[i]) {
+                call.setCancellation(cancellationPattern[i]);
+                call.setDepartureStatus(CallStatusEnumeration.CANCELLED);
+                call.setArrivalStatus(CallStatusEnumeration.CANCELLED);
+            } else {
+                call.setDepartureStatus(CallStatusEnumeration.ON_TIME);
+                call.setArrivalStatus(CallStatusEnumeration.ON_TIME);
+            }
+
+            estimatedCalls.getEstimatedCalls().add(call);
+        }
+
+        element.setEstimatedCalls(estimatedCalls);
+        element.setRecordedAtTime(ZonedDateTime.now());
+
+
+        return new SiriObjectFactory(Instant.now()).createETServiceDelivery(Arrays.asList(element));
+    }
+}
