33928d17b5e918ada49d2799104a6cf19c5a262a
Lasse Tyrihjell <lassetyr@gmail.com>
2018-04-12 15:26:49 +0200

    Fixing xpath

--- src/main/java/no/rutebanken/anshar/routes/validation/validators/Constants.java
+++ src/main/java/no/rutebanken/anshar/routes/validation/validators/Constants.java
@@ -4,5 +4,5 @@ class Constants {
     private static final String SERVICE_DELIVERY = "Siri/ServiceDelivery/";
     static final String PT_SITUATION_ELEMENT = SERVICE_DELIVERY + "SituationExchangeDelivery/Situations/PtSituationElement";
     static final String ESTIMATED_VEHICLE_JOURNEY = SERVICE_DELIVERY + "EstimatedTimetableDelivery/EstimatedJourneyVersionFrame/EstimatedVehicleJourney";
-    static final String ESTIMATED_CALL = ESTIMATED_VEHICLE_JOURNEY + "EstimatedCalls/EstimatedCall";
+    static final String ESTIMATED_CALL = ESTIMATED_VEHICLE_JOURNEY + "/EstimatedCalls/EstimatedCall";
 }
