5fd5239fa7ecdb90ff4d0f0737e8a961d30f9e28
Lasse Tyrihjell <lassetyr@gmail.com>
2019-11-15 12:46:48 +0100

    @WIP: Removing Cloud Pubsub support for now

--- src/main/java/no/rutebanken/anshar/App.java
+++ src/main/java/no/rutebanken/anshar/App.java
@@ -15,16 +15,14 @@
 
 package no.rutebanken.anshar;
 
-import org.entur.pubsub.camel.config.GooglePubSubCamelComponentConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 /**
  * A spring-boot application that includes a Camel route builder to setup the Camel routes
  */
 @SpringBootApplication
-@Import(GooglePubSubCamelComponentConfig.class)
+//@Import(GooglePubSubCamelComponentConfig.class)
 public class App {
 
     // must have a main method spring-boot can run
