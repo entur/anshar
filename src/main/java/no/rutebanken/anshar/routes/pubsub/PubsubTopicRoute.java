398d02342a5dcd9b83f49fc4e17ce8ddb748246f
Lasse Tyrihjell <lassetyr@gmail.com>
2019-11-22 11:43:34 +0100

    Logging simple pubsub-metrics

--- src/main/java/no/rutebanken/anshar/routes/pubsub/PubsubTopicRoute.java
+++ src/main/java/no/rutebanken/anshar/routes/pubsub/PubsubTopicRoute.java
@@ -4,11 +4,15 @@ import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Service
 public class PubsubTopicRoute extends RouteBuilder {
 
     @Value("${anshar.outbound.camel.route.topic.et.name}")
     private String etPubsubTopic;
+
+    private AtomicInteger counter = new AtomicInteger();
 
     @Override
     public void configure() {
@@ -17,8 +21,23 @@ public class PubsubTopicRoute extends RouteBuilder {
                 .to("xslt:xsl/splitAndFilterNotMonitored.xsl")
                 .split().tokenizeXML("Siri").streaming()
                 .to("direct:map.jaxb.to.protobuf")
+                .wireTap("direct:log.pubsub.traffic")
                 .to(etPubsubTopic)
         ;
+
+        from("direct:log.pubsub.traffic")
+                .routeId("log.pubsub")
+                .process(p -> {
+                    if (counter.incrementAndGet() % 1000 == 0) {
+                        p.getOut().setHeader("counter", counter.get());
+                        p.getOut().setBody(p.getIn().getBody());
+                    }
+                })
+                .choice()
+                .when(header("counter").isNotNull())
+                .log("Pubsub: Published ${header.counter} updates")
+                .endChoice()
+                .end();
     }
 }
 
