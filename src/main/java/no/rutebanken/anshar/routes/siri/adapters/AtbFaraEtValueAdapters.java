0688b0e95948a7623a8043cd5d264a3fbc365127
Lasse Tyrihjell <lassetyr@gmail.com>
2020-01-24 11:17:03 +0100

    Added separate mappingrules for Fara-ATB

---
+++ src/main/java/no/rutebanken/anshar/routes/siri/adapters/AtbFaraEtValueAdapters.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ *   https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
+
+package no.rutebanken.anshar.routes.siri.adapters;
+
+import no.rutebanken.anshar.routes.siri.transformer.ValueAdapter;
+import no.rutebanken.anshar.routes.siri.transformer.impl.StopPlaceRegisterMapper;
+import no.rutebanken.anshar.subscription.SubscriptionSetup;
+import uk.org.siri.siri20.DestinationRef;
+import uk.org.siri.siri20.JourneyPlaceRefStructure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mapping(id="atbfara")
+public class AtbFaraEtValueAdapters extends MappingAdapter {
+
+
+    @Override
+    public List<ValueAdapter> getValueAdapters(SubscriptionSetup subscriptionSetup) {
+
+        List<ValueAdapter> valueAdapters = new ArrayList<>();
+
+        valueAdapters.add(new StopPlaceRegisterMapper(
+                subscriptionSetup.getSubscriptionType(),
+                subscriptionSetup.getDatasetId(),
+                JourneyPlaceRefStructure.class,
+                subscriptionSetup.getIdMappingPrefixes(),
+                "Quay"));
+
+        valueAdapters.add(new StopPlaceRegisterMapper(
+                subscriptionSetup.getSubscriptionType(),
+                subscriptionSetup.getDatasetId(),
+                DestinationRef.class,
+                subscriptionSetup.getIdMappingPrefixes(),
+                "Quay"));
+
+        if (subscriptionSetup.getDatasetId() != null && !subscriptionSetup.getDatasetId().isEmpty()) {
+            List<ValueAdapter> datasetPrefix = createIdPrefixAdapters(subscriptionSetup.getDatasetId());
+            if (!subscriptionSetup.getMappingAdapters().containsAll(datasetPrefix)) {
+                subscriptionSetup.getMappingAdapters().addAll(datasetPrefix);
+            }
+        }
+
+        return valueAdapters;
+    }
+}
