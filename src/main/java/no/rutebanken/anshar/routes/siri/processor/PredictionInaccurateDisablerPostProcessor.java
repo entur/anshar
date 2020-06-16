866c4bce744db45f899f031f7a0383dba625e93c
Lasse Tyrihjell <lassetyr@gmail.com>
2020-06-16 10:43:34 +0200

    Adding adapter to override PredictionInaccurate on departure-level

---
+++ src/main/java/no/rutebanken/anshar/routes/siri/processor/PredictionInaccurateDisablerPostProcessor.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ *   https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
+
+package no.rutebanken.anshar.routes.siri.processor;
+
+import no.rutebanken.anshar.routes.siri.transformer.ValueAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.org.siri.siri20.EstimatedTimetableDeliveryStructure;
+import uk.org.siri.siri20.EstimatedVehicleJourney;
+import uk.org.siri.siri20.EstimatedVersionFrameStructure;
+import uk.org.siri.siri20.Siri;
+
+import java.util.List;
+
+/**
+ * Overrides the PredictionInaccurate on departure-level by setting it to <code>false</code> always
+ *
+ */
+public class PredictionInaccurateDisablerPostProcessor extends ValueAdapter implements PostProcessor {
+    private static final Logger logger = LoggerFactory.getLogger(PredictionInaccurateDisablerPostProcessor.class);
+
+    @Override
+    public void process(Siri siri) {
+
+        List<EstimatedTimetableDeliveryStructure> etDeliveries = siri.getServiceDelivery().getEstimatedTimetableDeliveries();
+        if (etDeliveries != null) {
+            for (EstimatedTimetableDeliveryStructure etDelivery : etDeliveries) {
+                List<EstimatedVersionFrameStructure> estimatedJourneyVersionFrames = etDelivery.getEstimatedJourneyVersionFrames();
+                for (EstimatedVersionFrameStructure estimatedJourneyVersionFrame : estimatedJourneyVersionFrames) {
+
+                    if (estimatedJourneyVersionFrame.getEstimatedVehicleJourneies() != null) {
+                        int counter = 0;
+                        for (EstimatedVehicleJourney estimatedVehicleJourney : estimatedJourneyVersionFrame.getEstimatedVehicleJourneies()) {
+                            if (Boolean.TRUE.equals(estimatedVehicleJourney.isPredictionInaccurate())) {
+                                estimatedVehicleJourney.setPredictionInaccurate(Boolean.FALSE);
+                                counter++;
+                            }
+                        }
+                        if (counter > 0) {
+                            logger.info("Overriding {} PredictionInaccurate-flags", counter);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    protected String apply(String value) {
+        return null;
+    }
+}
---
+++ src/test/java/no/rutebanken/anshar/siri/processor/PredictionInaccurateDisablerPostProcessorTest.java
@@ -0,0 +1,69 @@
+package no.rutebanken.anshar.siri.processor;
+
+import no.rutebanken.anshar.routes.siri.helpers.SiriObjectFactory;
+import no.rutebanken.anshar.routes.siri.processor.PredictionInaccurateDisablerPostProcessor;
+import org.junit.Test;
+import uk.org.siri.siri20.EstimatedVehicleJourney;
+import uk.org.siri.siri20.Siri;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNull;
+
+public class PredictionInaccurateDisablerPostProcessorTest {
+
+    PredictionInaccurateDisablerPostProcessor processor = new PredictionInaccurateDisablerPostProcessor();
+
+    @Test
+    public void testOverrideWithTrueValue() {
+
+        Siri siri = createSimpleEt(Boolean.TRUE);
+
+        //Assert correctly set value
+        assertEquals(Boolean.TRUE, siri.getServiceDelivery().getEstimatedTimetableDeliveries().get(0).getEstimatedJourneyVersionFrames().get(0).getEstimatedVehicleJourneies().get(0).isPredictionInaccurate());
+
+        processor.process(siri);
+
+        //Assert that value has been set to false
+        assertEquals(Boolean.FALSE, siri.getServiceDelivery().getEstimatedTimetableDeliveries().get(0).getEstimatedJourneyVersionFrames().get(0).getEstimatedVehicleJourneies().get(0).isPredictionInaccurate());
+    }
+
+    @Test
+    public void testOverrideWithFalseValue() {
+
+        Siri siri = createSimpleEt(Boolean.FALSE);
+
+        //Assert correctly set value
+        assertEquals(Boolean.FALSE, siri.getServiceDelivery().getEstimatedTimetableDeliveries().get(0).getEstimatedJourneyVersionFrames().get(0).getEstimatedVehicleJourneies().get(0).isPredictionInaccurate());
+
+        processor.process(siri);
+
+        //Assert that value has been set to false
+        assertEquals(Boolean.FALSE, siri.getServiceDelivery().getEstimatedTimetableDeliveries().get(0).getEstimatedJourneyVersionFrames().get(0).getEstimatedVehicleJourneies().get(0).isPredictionInaccurate());
+    }
+
+    @Test
+    public void testOverrideWithNullValue() {
+
+        Siri siri = createSimpleEt(null);
+
+        //Assert correctly set value
+        assertNull(siri.getServiceDelivery().getEstimatedTimetableDeliveries().get(0).getEstimatedJourneyVersionFrames().get(0).getEstimatedVehicleJourneies().get(0).isPredictionInaccurate());
+
+        processor.process(siri);
+
+        //Assert that value has been set to false
+        assertNull(siri.getServiceDelivery().getEstimatedTimetableDeliveries().get(0).getEstimatedJourneyVersionFrames().get(0).getEstimatedVehicleJourneies().get(0).isPredictionInaccurate());
+    }
+
+    private Siri createSimpleEt(Boolean predictionInaccurate) {
+        List<EstimatedVehicleJourney> etList = new ArrayList<>();
+        EstimatedVehicleJourney et = new EstimatedVehicleJourney();
+        et.setPredictionInaccurate(predictionInaccurate);
+        etList.add(et);
+        return new SiriObjectFactory(Instant.now()).createETServiceDelivery(etList);
+    }
+}
