37c19a2ac9898b19e2473a44f36717556cc7a43a
Lasse Tyrihjell <lassetyr@gmail.com>
2021-03-12 13:08:13 +0100

    Adding preliminary support for publishing data to Kafka

---
+++ src/main/java/no/rutebanken/anshar/routes/kafka/KafkaPublisher.java
@@ -0,0 +1,110 @@
+/*
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ *  https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ *
+ */
+
+package no.rutebanken.anshar.routes.kafka;
+
+import net.logstash.logback.encoder.org.apache.commons.lang3.StringUtils;
+import org.apache.camel.component.kafka.KafkaConfiguration;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.Map;
+import java.util.Properties;
+
+@Service
+public class KafkaPublisher {
+    private final Logger log = LoggerFactory.getLogger(KafkaPublisher.class);
+
+    @Value("${anshar.kafka.enabled:false}")
+    private boolean kafkaEnabled;
+
+    @Value("${anshar.kafka.security.protocol}")
+    private String securityProtocol;
+
+    @Value("${anshar.kafka.security.sasl.mechanism}")
+    private String saslMechanism;
+
+    @Value("${anshar.kafka.sasl.username}")
+    private String saslUsername;
+
+    @Value("${anshar.kafka.sasl.password}")
+    private String saslPassword;
+
+    @Value("${anshar.kafka.brokers}")
+    private String brokers;
+
+    @Value("${anshar.kafka.clientId:}")
+    private String clientId;
+
+    @Value("${anshar.kafka.compressionType:gzip}")
+    private String compressionType;
+
+    private KafkaProducer producer;
+
+    @PostConstruct
+    public void init() {
+        if (!kafkaEnabled) {
+            return;
+        }
+
+        // Using default configuration as suggested by Camel
+        KafkaConfiguration config = new KafkaConfiguration();
+
+        Properties properties = config.createProducerProperties();
+
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+        properties.put(ProducerConfig.CLIENT_ID_CONFIG, clientId);
+        properties.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "gzip");
+
+        // Security
+        if (!StringUtils.isEmpty(saslUsername) && !StringUtils.isEmpty(saslPassword)) {
+            properties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, securityProtocol);
+
+            properties.put(SaslConfigs.SASL_MECHANISM, saslMechanism);
+            String jaasConfigContents = "org.apache.kafka.common.security.scram.ScramLoginModule required\nusername=\"%s\"\npassword=\"%s\";";
+            properties.put(
+                SaslConfigs.SASL_JAAS_CONFIG,
+                String.format(jaasConfigContents, saslUsername.trim(), saslPassword.trim())
+            );
+        } else {
+            log.info("Skip Kafka-authentication as no user/passowrd is set");
+        }
+        producer = new KafkaProducer(properties);
+
+    }
+
+    public void publishToKafka(String topicName, String siriData, Map metadataHeaders) {
+        if (!kafkaEnabled) {
+            log.debug("Push to Kafka is disabled, should have pushed update ");
+            return;
+        }
+
+        if (producer != null) {
+            //Fire and forget
+            producer.send(new ProducerRecord(topicName, siriData, metadataHeaders));
+        }
+
+    }
+
+}
---
+++ src/main/java/no/rutebanken/anshar/routes/kafka/KafkaPublisherRoute.java
@@ -0,0 +1,60 @@
+package no.rutebanken.anshar.routes.kafka;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaPublisherRoute extends RouteBuilder {
+
+    @Value("${anshar.kafka.topic.et.name:}")
+    private String kafkaEtTopic;
+
+    @Value("${anshar.kafka.topic.vm.name:}")
+    private String kafkaVmTopic;
+
+    @Value("${anshar.kafka.topic.sx.name:}")
+    private String kafkaSxTopic;
+
+    @Autowired
+    private KafkaPublisher publisher;
+
+    @Override
+    public void configure() throws Exception {
+
+        /*
+          TODO: Add support for metadata-headers for easier filtering in Kafka?
+                e.g.:
+                    - codespace
+                    - lineRef
+                    - serviceJourneyId
+                    - ???
+                    - breadcrumbId?
+         */
+
+
+        from("direct:kafka.et.xml")
+            .choice().when(e -> !StringUtils.isEmpty(kafkaEtTopic))
+                .setHeader("topic", simple(kafkaEtTopic))
+                .bean(publisher, "publishToKafka(${header.topic}, ${body})")
+            .endChoice()
+            .routeId("anshar.kafka.et.producer");
+
+        from("direct:kafka.vm.xml")
+            .choice().when(e -> !StringUtils.isEmpty(kafkaVmTopic))
+                .setHeader("topic", simple(kafkaVmTopic))
+                .bean(publisher, "publishToKafka(${header.topic}, ${body})")
+            .endChoice()
+            .routeId("anshar.kafka.vm.producer");
+
+        from("direct:kafka.sx.xml")
+            .choice().when(e -> !StringUtils.isEmpty(kafkaSxTopic))
+                .setHeader("topic", simple(kafkaSxTopic))
+                .bean(publisher, "publishToKafka(${header.topic}, ${body})")
+            .endChoice()
+            .routeId("anshar.kafka.sx.producer");
+
+    }
+}
