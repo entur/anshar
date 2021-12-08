0d19e37cf19820bcc07e215b18e507d7a5ed10b7
Lasse Tyrihjell <lassetyr@gmail.com>
2021-12-08 12:46:47 +0100

    Add default adapters to all subscriptions - also if no other adapters are defined

--- src/main/java/no/rutebanken/anshar/subscription/SubscriptionInitializer.java
+++ src/main/java/no/rutebanken/anshar/subscription/SubscriptionInitializer.java
@@ -17,23 +17,10 @@ package no.rutebanken.anshar.subscription;
 
 import com.google.common.base.Preconditions;
 import no.rutebanken.anshar.config.AnsharConfiguration;
-import no.rutebanken.anshar.routes.siri.Siri20ToSiriRS14Subscription;
-import no.rutebanken.anshar.routes.siri.Siri20ToSiriRS20RequestResponse;
-import no.rutebanken.anshar.routes.siri.Siri20ToSiriRS20Subscription;
-import no.rutebanken.anshar.routes.siri.Siri20ToSiriWS14RequestResponse;
-import no.rutebanken.anshar.routes.siri.Siri20ToSiriWS14Subscription;
-import no.rutebanken.anshar.routes.siri.Siri20ToSiriWS20RequestResponse;
-import no.rutebanken.anshar.routes.siri.Siri20ToSiriWS20Subscription;
+import no.rutebanken.anshar.routes.siri.*;
 import no.rutebanken.anshar.routes.siri.adapters.Mapping;
 import no.rutebanken.anshar.routes.siri.handlers.SiriHandler;
-import no.rutebanken.anshar.routes.siri.processor.AddOrderToAllCallsPostProcessor;
-import no.rutebanken.anshar.routes.siri.processor.CodespaceProcessor;
-import no.rutebanken.anshar.routes.siri.processor.EnsureIncreasingTimesForCancelledStopsProcessor;
-import no.rutebanken.anshar.routes.siri.processor.EnsureNonNullVehicleModePostProcessor;
-import no.rutebanken.anshar.routes.siri.processor.ExtraJourneyDestinationDisplayPostProcessor;
-import no.rutebanken.anshar.routes.siri.processor.ExtraJourneyPostProcessor;
-import no.rutebanken.anshar.routes.siri.processor.RemovePersonalInformationProcessor;
-import no.rutebanken.anshar.routes.siri.processor.ReportTypeProcessor;
+import no.rutebanken.anshar.routes.siri.processor.*;
 import no.rutebanken.anshar.routes.siri.transformer.ApplicationContextHolder;
 import no.rutebanken.anshar.routes.siri.transformer.ValueAdapter;
 import no.rutebanken.anshar.subscription.helpers.RequestType;
@@ -47,13 +34,7 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.ServiceConfigurationError;
-import java.util.Set;
+import java.util.*;
 
 @Component
 public class SubscriptionInitializer implements CamelContextAware {
@@ -141,28 +122,29 @@ public class SubscriptionInitializer implements CamelContextAware {
                     throw new ServiceConfigurationError("InternalId is NOT unique for ID="+subscriptionSetup.getInternalId());
                 }
 
+                List<ValueAdapter> valueAdapters = new ArrayList<>();
+                //Is added to ALL subscriptions
+                valueAdapters.add(new CodespaceProcessor(subscriptionSetup.getDatasetId()));
+                valueAdapters.add(new ReportTypeProcessor(subscriptionSetup.getDatasetId()));
+                valueAdapters.add(new EnsureIncreasingTimesForCancelledStopsProcessor(subscriptionSetup.getDatasetId()));
+                valueAdapters.add(new RemovePersonalInformationProcessor());
+                valueAdapters.add(new ExtraJourneyDestinationDisplayPostProcessor(subscriptionSetup.getDatasetId()));
+                valueAdapters.add(new AddOrderToAllCallsPostProcessor(subscriptionSetup.getDatasetId()));
+
+                valueAdapters.add(new EnsureNonNullVehicleModePostProcessor());
+                valueAdapters.add(new ExtraJourneyPostProcessor(subscriptionSetup.getDatasetId()));
+
 
                 if (mappingAdaptersById.containsKey(subscriptionSetup.getMappingAdapterId())) {
                     Class adapterClass = mappingAdaptersById.get(subscriptionSetup.getMappingAdapterId());
                     try {
-                        List<ValueAdapter> valueAdapters = (List<ValueAdapter>) adapterClass.getMethod("getValueAdapters", SubscriptionSetup.class).invoke(adapterClass.newInstance(), subscriptionSetup);
+                        valueAdapters.addAll((List<ValueAdapter>) adapterClass.getMethod("getValueAdapters", SubscriptionSetup.class).invoke(adapterClass.newInstance(), subscriptionSetup));
 
-                        //Is added to ALL subscriptions
-                        valueAdapters.add(new CodespaceProcessor(subscriptionSetup.getDatasetId()));
-                        valueAdapters.add(new ReportTypeProcessor(subscriptionSetup.getDatasetId()));
-                        valueAdapters.add(new EnsureIncreasingTimesForCancelledStopsProcessor(subscriptionSetup.getDatasetId()));
-                        valueAdapters.add(new RemovePersonalInformationProcessor());
-                        valueAdapters.add(new ExtraJourneyDestinationDisplayPostProcessor(subscriptionSetup.getDatasetId()));
-                        valueAdapters.add(new AddOrderToAllCallsPostProcessor(subscriptionSetup.getDatasetId()));
-
-                        valueAdapters.add(new EnsureNonNullVehicleModePostProcessor());
-                        valueAdapters.add(new ExtraJourneyPostProcessor(subscriptionSetup.getDatasetId()));
-
-                        subscriptionSetup.getMappingAdapters().addAll(valueAdapters);
                     } catch (Exception e) {
                         throw new ServiceConfigurationError("Invalid mappingAdapterId for subscription " + subscriptionSetup, e);
                     }
                 }
+                subscriptionSetup.getMappingAdapters().addAll(valueAdapters);
 
                 if (subscriptionSetup.getSubscriptionMode() == SubscriptionSetup.SubscriptionMode.FETCHED_DELIVERY |
                         subscriptionSetup.getSubscriptionMode() == SubscriptionSetup.SubscriptionMode.POLLING_FETCHED_DELIVERY) {
