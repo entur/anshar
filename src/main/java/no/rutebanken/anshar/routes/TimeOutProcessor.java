47d9b1cddfb076066a3e5384308eff730ef6f343
Lasse Tyrihjell <lassetyr@gmail.com>
2021-12-20 16:38:32 +0100

    More logging when request times out

--- src/main/java/no/rutebanken/anshar/routes/TimeOutProcessor.java
+++ src/main/java/no/rutebanken/anshar/routes/TimeOutProcessor.java
@@ -3,6 +3,9 @@ package no.rutebanken.anshar.routes;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.ProducerTemplate;
+import org.jboss.logging.MDC;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -15,6 +18,8 @@ import java.util.concurrent.TimeoutException;
  */
 public class TimeOutProcessor implements Processor {
 
+    private final Logger logger = LoggerFactory.getLogger(TimeOutProcessor.class);
+
     private String route;
     private long timeoutMillis;
 
@@ -25,6 +30,9 @@ public class TimeOutProcessor implements Processor {
 
     @Override
     public void process(Exchange exchange) throws Exception {
+
+        MDC.put(Exchange.BREADCRUMB_ID, exchange.getIn().getHeader(Exchange.BREADCRUMB_ID));
+
         Future<Exchange> future = null;
         ProducerTemplate producerTemplate = exchange.getFromEndpoint().getCamelContext().createProducerTemplate();
         try {
@@ -35,9 +43,12 @@ public class TimeOutProcessor implements Processor {
             producerTemplate.stop();
             future.cancel(true);
         } catch (TimeoutException e) {
+            logger.info("Request timed out after " + timeoutMillis + " ms - aborted.", e);
             producerTemplate.stop();
             future.cancel(true);
             throw new TimeoutException("Caught timeout while processing route: " + route);
+        } finally {
+            MDC.remove(Exchange.BREADCRUMB_ID);
         }
 
     }
