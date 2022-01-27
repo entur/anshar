23124163dd90a3e97fdf4eba715dac996d077b74
Lasse Tyrihjell <lassetyr@gmail.com>
2022-01-27 08:37:20 +0100

    If SX-message does not exist - always flag as updated

--- src/main/java/no/rutebanken/anshar/data/Situations.java
+++ src/main/java/no/rutebanken/anshar/data/Situations.java
@@ -322,8 +322,7 @@ public class Situations extends SiriRepository<PtSituationElement> {
             String existingChecksum = checksumCache.get(key);
             timingTracer.mark("checksumCache.get");
             boolean updated;
-//            if (existingChecksum != null && situationElements.containsKey(key)) { // Checksum not compared if actual situation does not exist
-            if (existingChecksum != null) {
+            if (existingChecksum != null && situationElements.containsKey(key)) { // Checksum not compared if actual situation does not exist
                 //Exists - compare values
                 updated =  !(currentChecksum.equals(existingChecksum));
             } else {
