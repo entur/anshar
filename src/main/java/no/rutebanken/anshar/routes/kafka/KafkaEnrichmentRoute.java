7a04bb831fa9709b15f4ae1601c9900e9a84314a
Lasse Tyrihjell <lassetyr@gmail.com>
2022-05-20 12:58:02 +0200

    Added excessive debug-logging for initial testing purposes

--- src/main/java/no/rutebanken/anshar/routes/kafka/KafkaEnrichmentRoute.java
+++ src/main/java/no/rutebanken/anshar/routes/kafka/KafkaEnrichmentRoute.java
@@ -42,12 +42,13 @@ public class KafkaEnrichmentRoute extends KafkaConfig {
 
             from("direct:anshar.enrich.siri.et")
                     .log("Adding to kafka-enrichment topic")
+                    .to("log:push:" + getClass().getSimpleName() + "?showAll=true&multiline=true")
                     .setHeader("topic", simple(kafkaEnrichEtTopic))
                     .removeHeader(INTERNAL_PUBLISH_TO_KAFKA_FOR_APC_ENRICHMENT)
                     .to("xslt-saxon:xsl/split.xsl")
                     .split().tokenizeXML("Siri").streaming()
                     .to(kafkaProducerConfig)
-                    .routeId("anshar.enrich.siri.et.kafka")
+                    .routeId("anshar.enrich.siri.et.kafka.producer")
             ;
 
             from(kafkaConsumerConfig)
@@ -63,12 +64,15 @@ public class KafkaEnrichmentRoute extends KafkaConfig {
                             }
                         }
                     })
+                    .to("log:kafka-consume:" + getClass().getSimpleName() + "?showAll=true&multiline=true")
                     .to("direct:send.to.queue")
+                    .routeId("anshar.enrich.siri.et.kafka.consumer")
             ;
 
         } else {
             // Immediately redirect to continue internal processing
             from("direct:anshar.enrich.siri.et")
+                .log("Redirecting internally")
                 .removeHeader(INTERNAL_PUBLISH_TO_KAFKA_FOR_APC_ENRICHMENT)
                 .to("direct:send.to.queue")
                 .routeId("anshar.enrich.siri.et.kafka.redirect")
