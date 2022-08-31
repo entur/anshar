944f5f2c1222e566232dd289640bd1452cfca3d6
Begerad, Stefan <dialog@swingbe.de>
2022-08-31 22:37:26 +0200

    chore: test failures

--- src/test/java/no/rutebanken/anshar/data/EstimatedTimetablesTest.java
+++ src/test/java/no/rutebanken/anshar/data/EstimatedTimetablesTest.java
@@ -43,7 +43,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 public class EstimatedTimetablesTest extends SpringBootBaseTest {
 
     @Autowired
